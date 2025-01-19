a811702d29771b471e5ed36e37b71bb39651dcd0
rajneesh verma <curiousrajneesh@2027gmail.com>
2025-01-19 13:01:35 +0530

    Files added

---
+++ src/Utils/constant.js
@@ -0,0 +1,25 @@
+export const LOGO = 
+    "https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg";
+
+export const USER_AVATAR = 
+    "https://wallpapers.com/images/hd/netflix-profile-pictures-1000-x-1000-qo9h82134t9nv0j0.jpg";
+
+export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+        accept: 'application/json',
+        Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhMjlkOTNjMGExNGZiZGVkMmQzM2QxMjFmMzgyODU1NyIsIm5iZiI6MTczNTQwNzEwNy4yMjIwMDAxLCJzdWIiOiI2NzcwMzYwM2Y5NDU0ZWViMWQ5MmQwYzciLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.87PB5LOHF6d1mVHq44mpeH7yguIkRqgMR-tBunphv0Y'
+    },
+};
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w300"
+
+export const BG_URL = "https://assets.nflxext.com/ffe/siteui/vlv3/e3e9c31f-aa15-4a8f-8059-04f01e6b8629/web/IN-en-20250113-TRIFECTA-perspective_febfa442-23d9-45f3-937e-72f8b971f7a9_small.jpg"
+
+
+export const SUPPORTED_LANGUAGES = [{identifier: "en", name: "English"},
+    {identifier: "hindi", name: "Hindi"},
+    {identifier: "spanish", name: "Spanish"},
+ ];
+
+export const GEMINI_KEY = "AIzaSyBf8PeuaAEVbpRLa-ZCm1kJE6VwoNpq22Q"
